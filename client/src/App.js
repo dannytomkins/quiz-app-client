222904319bc5c5425bf228a49cbed5ae7d39a1c5
Danny Tomkins <tomkinsdan@gmail.com>
2021-10-11 14:09:41 -0500

    import register and login at app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Register from './components/Register';
+import Login from './components/Login';
+
 import './App.css';
 
 function App() {
