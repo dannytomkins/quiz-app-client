55d68271d8bf86e728bdc7869995e06248836dfd
Sean Monaghan <smonagha@conncoll.edu>
2021-10-15 00:28:17 -0400

    added post score functionality upon completion of test

--- client/src/actions/score.js
+++ client/src/actions/score.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const host = "http://localhost:2121";
 
+    export const postScore = (score) => {
+        return axios.post(host + "/score", score)
+    }
+
 
     export const getScoresByUserId = (id) => {
         return axios.get(host + "/score/" + id);
--- client/src/components/Quiz/Quiz.js
+++ client/src/components/Quiz/Quiz.js
@@ -5,50 +5,12 @@ import Button from '@mui/material/Button';
 import { getQuizById } from "../../actions/quiz";
 import { useParams } from "react-router-dom";
 import { Typography } from "@mui/material";
+import { postScore } from "../../actions/score"
+import Cookies from "js-cookie";
 
 const Quiz = () => {
-    // console.log(props);
 
-    // const questions = [
-	// 	{
-	// 		questionText: 'What is the capital of France?',
-	// 		answerOptions: [
-	// 			{ answerText: 'New York', isCorrect: false },
-	// 			{ answerText: 'London', isCorrect: false },
-	// 			{ answerText: 'Paris', isCorrect: true },
-	// 			{ answerText: 'Dublin', isCorrect: false },
-	// 		],
-	// 	},
-	// 	{
-	// 		questionText: 'Who is CEO of Tesla?',
-	// 		answerOptions: [
-	// 			{ answerText: 'Jeff Bezos', isCorrect: false },
-	// 			{ answerText: 'Elon Musk', isCorrect: true },
-	// 			{ answerText: 'Bill Gates', isCorrect: false },
-	// 			{ answerText: 'Tony Stark', isCorrect: false },
-	// 		],
-	// 	},
-	// 	{
-	// 		questionText: 'The iPhone was created by which company?',
-	// 		answerOptions: [
-	// 			{ answerText: 'Apple', isCorrect: true },
-	// 			{ answerText: 'Intel', isCorrect: false },
-	// 			{ answerText: 'Amazon', isCorrect: false },
-	// 			{ answerText: 'Microsoft', isCorrect: false },
-	// 		],
-	// 	},
-	// 	{
-	// 		questionText: 'How many Harry Potter books are there?',
-	// 		answerOptions: [
-	// 			{ answerText: '1', isCorrect: false },
-	// 			{ answerText: '4', isCorrect: false },
-	// 			{ answerText: '6', isCorrect: false },
-	// 			{ answerText: '7', isCorrect: true },
-	// 		],
-	// 	},
-	// ];
 	const quizId = useParams();
-	// const {id} = quizId
 	console.log("param:", quizId);
 
 
@@ -59,9 +21,93 @@ const Quiz = () => {
 			"category:" : "",
 			"level" : "",
 			"userId" : "",
-			"question" : []
+			"question" : [{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},
+			{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			},{
+				"id" : "",
+				"quizId" : "",
+				"question" : "",
+				"correctAnswer" : "",
+				"wrongAnswerOne" : "",
+				"wrongAnswerTwo" : "",
+				"wrongAnswerThree" : ""
+			}
+		]
 		}
 		);
+
+		
 
 	useEffect(() => {
 		
@@ -72,113 +118,102 @@ const Quiz = () => {
 
 	  console.log("Quiz: ", quiz)
 
-    // const [currentQuestion, setCurrentQuestion] = useState(0);
-	// const [showScore, setShowScore] = useState(false);
-	// const [score, setScore] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
 
-    // const handleAnswerOptionClick = (isCorrect) => {
-    //     if (isCorrect) {
-    //         setScore(score + 1);
-    //     }
+    const handleAnswerOptionClick = (answer) => {
+        if (answer == 'correctAnswer') {
+            setScore(score + 1);
+        }
 
-    //     const nextQuestion = currentQuestion+1;
-    //     if (nextQuestion < questions.length) {
-    //         setCurrentQuestion(nextQuestion);
-    //     } else {
-    //         setShowScore(true);
-    //     }
-    // };
+        const nextQuestion = currentQuestion+1;
+        if (nextQuestion < quiz.question.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+			postScore({
+				"userId" : Cookies.get("id"),
+				"quizId" : quiz.id,
+				"score" : score + 1
+			})
+        }
+    };
 
     return (
-		
 		<div>
-		{quiz.question.length > 0 ? (
-		 	quiz.question.map((question) => (
-		 	  <Card variant='outlined'>
-		 		<CardContent>
-		 		  <Typography variant='h5' component='div'>
-		 			{question.id}
-		 		  </Typography>
-		 		  <Typography variant='h3'>
-		 			{question.id}
-		 			{'%'}
-		 		  </Typography>
-		 		</CardContent>
-		 	  </Card>
-		 	))
-		   ) : (
-		 	<p>No scores found</p>
-		   )}
-
-			{/* {quiz.data.question.map(question => {
-				return (
-					<p>{question.correctAnswer}</p>
-				)
-			})}
 			{showScore ? (
+			<Card>
+				<CardContent>
+					<Typography variant="h4">You scored {score} out of {quiz.question.length}</Typography>
+
+				</CardContent>
+			</Card>
+		) : (
+			<>
+				<div>
+					<Card>
+						<CardContent>
+							<Typography variant = "h5">
+								Question {currentQuestion + 1} / {quiz.question.length}
+							</Typography>
+						</CardContent>
+					</Card>
+					<Card>
+						<CardContent>
+							<Typography variant = "h2">
+								{quiz.question[currentQuestion].question}
+							</Typography>
+
+						</CardContent>
+					</Card>
+					</div>
+				<div>
 				<Card>
-                    <CardContent>
-                        You scored {score} out of {questions.length}
-                    </CardContent>
-                </Card>
-			) : (
-				<>
-					<div>
-						<Card>
-                            <CardContent>
-							    <span>Question {currentQuestion + 1}</span>/{questions.length}
-                            </CardContent>
-						</Card>
-                        <Card>
-                            <CardContent>
-                                <div className='question-text'>{questions[currentQuestion].questionText}</div>
-                            </CardContent>
-						</Card>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<Button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</Button>
-						))}
-					</div>
-				</>
-			)} */}
-		</div>
+					<CardContent>	
+						<Button variant = "overline"  sx = {{ fontSize: 16, width : "80%"}} color = "text.secondary" onClick = {() => handleAnswerOptionClick('correctAnswer')}>
+							<Typography variant = "h6">
+								{quiz.question[currentQuestion].correctAnswer}
+							</Typography>
+						</Button>
+					</CardContent>
+				</Card>
+				<br/>
+				<Card>
+					<CardContent>
+						<Button variant = "overline" sx = {{ fontSize: 16, width : "80%"}} color = "text.secondary" onClick = {() => handleAnswerOptionClick('incorrectAnswer')}>
+							<Typography variant = "h6">	
+								{quiz.question[currentQuestion].wrongAnswerOne}
+							</Typography>
+						</Button>
+					</CardContent>
+				</Card>
+					<br />
+					<Card>
+						<CardContent>
+							<Button variant = "overline" sx = {{ fontSize: 16, width : "80%"}} color = "text.secondary" onClick = {() => handleAnswerOptionClick('incorrectAnswer')}>
+								<Typography variant = "h6">
+									{quiz.question[currentQuestion].wrongAnswerTwo}
+								</Typography>	
+						</Button>
+					</CardContent>
+				</Card>
+					<br/>
+					<Card>
+						<CardContent>
+							<Button variant = "overline" sx = {{ fontSize: 16, width : "80%"}} color = "text.secondary" onClick = {() => handleAnswerOptionClick('incorrectAnswer')}>
+								<Typography variant = "h6">
+									{quiz.question[currentQuestion].wrongAnswerThree}
+								</Typography>
+							</Button>
+						</CardContent>
+					</Card>
+					<br />
+				</div>
+			</>
+		)}
+		</div>	 	
 	);
-
-
-
-
-    // return (
-    //     <div>
-    //     {/* {props.questions.map(question => { */}
-    //     <Card variant = "outlined" sx={{ width: '80%', mx: 'auto', mb: '10px'}}>
-    //         <CardContent>
-    //             <Typography variant = "h3" color = "text.primary" gutterBottom>
-    //                 Question number 'numbered index'
-    //             </Typography>
-    //             <br />
-    //             <Typography variant = "overline" sx = {{ fontSize: 20}} color = "text.secondary">
-    //                 Answer One'[0]'
-    //             </Typography>
-    //             <br/>
-    //             <Typography variant = "overline" sx = {{ fontSize: 20}} color = "text.secondary">
-    //                 Answer Two'[0]'
-    //             </Typography>
-    //             <br />
-    //             <Typography variant = "overline" sx = {{ fontSize: 20}} color = "text.secondary">
-    //                 Answer Three'[0]'
-    //             </Typography>
-    //             <br/>
-    //             <Typography variant = "overline" sx = {{ fontSize: 20}} color = "text.secondary">
-    //                 Answer Four'[0]'
-    //             </Typography>
-    //             <br />
-    //         </CardContent>
-    //     </Card>
-    //     {/* }) */}
-    // {/* } */}
-    // </div>
-    // )
 }
 
 export default Quiz
