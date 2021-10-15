b80a95b3d6390ef465a1bf26e19ff9fbbff92dc2
Danny Tomkins <tomkinsdan@gmail.com>
2021-10-15 14:27:10 -0500

    updated QuizScores to include score quiz object

--- client/src/components/Profile/QuizScores.js
+++ client/src/components/Profile/QuizScores.js
@@ -1,47 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
+import { Link } from "react-router-dom";
+
 
 import { getScoresByUserId } from '../../actions/score';
-import { getAllScores } from '../../actions/score';
-import { getQuizById } from '../../actions/quiz';
+// import { getAllScores } from '../../actions/score';
+// import { getQuizById } from '../../actions/quiz';
+// import { getAllQuizzes } from '../../actions/quiz';
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
 
 const QuizScores = () => {
   const id = Cookies.get('id');
 
   const [score, setScore] = useState([])
-  const [quiz, setQuiz] = useState([])
-
-
-  // useEffect(() => {
-  //   getScoresByUserId(id).then(score => {setScore(score.data)});
-  // }, []);
 
   useEffect(() => {
-    getAllScores()
-    .then(score => {setScore(score.data)})
-    .then(score.forEach(score => {
-      getQuizById(score.quizId)
-      .then(quiz => {setQuiz(quiz.data)})
-    }));
-    console.log("quiz: ", quiz);
-
+    getScoresByUserId(id)
+    .then(score => {setScore(score.data)});
   }, []);
-
-  
-
-  // const score = [
-  //   { quizId: 1, scorePercent: 100 },
-  //   { quizId: 33, scorePercent: 80 },
-  //   { quizId: 90, scorePercent: 40 },
-  // ];
-
-  console.log("score: ", score);
-  console.log("quiz: ", quiz);
-
 
   return (
     <div>
@@ -49,16 +29,17 @@ const QuizScores = () => {
       {console.log(score)}
       {score.length > 0 ? (
         score.map((score) => (
-          <Card variant='outlined'>
+          <Card variant = "outlined" sx={{ width: '80%', mx: 'auto', mb: '10px'}}>
             <CardContent>
               <Typography variant='h5' component='div'>
-                {/* Change this to quiz.quizname when available */}
-                {score.quizId}
+                {score.quiz.quizName}
               </Typography>
               <Typography variant='h3'>
-                {score.score}
-                {'%'}
+                {score.score} / {score.quiz.question.length}
               </Typography>
+              <Button variant='h5' component='div' component = {Link} to = {`/quiz/${score.quiz.id}`}>
+              Try again?
+              </Button>
             </CardContent>
           </Card>
         ))
